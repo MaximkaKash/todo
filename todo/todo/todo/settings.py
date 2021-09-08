7a78d42b7d22cffcc598a194271fcd8adc027897
MaximkaKash <kanashitsusu@gamil.com>
2021-09-08 20:10:09 +0300

    End!

--- todo/todo/todo/settings.py
+++ todo/todo/todo/settings.py
@@ -75,11 +75,16 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   "default": {
+       "ENGINE": "django.db.backends.postgresql",
+       "NAME": "todo",
+       "USER": "todo",
+       "PASSWORD": "todo",
+       "HOST": "localhost",
+       "PORT": 5432,
+   }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
